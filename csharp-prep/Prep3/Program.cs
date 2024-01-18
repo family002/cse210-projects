6eac0c21af4cfb24e93904529c29148b9ad5d43a
family002 <adama8001@outlook.com>
2024-01-17 20:26:05 -0700

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        string game = "yes";
+        Random random = new Random();
+        int number = random.Next(1,101);
+        while (game == "yes")
+            {
+                Console.Write("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    game = "no";
+                }
+            }
     }
 }
