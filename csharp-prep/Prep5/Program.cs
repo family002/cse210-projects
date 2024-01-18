e60c08ad8f1b0c0f27bee58b1f9ac720a5e17e53
family002 <adama8001@outlook.com>
2024-01-17 22:59:15 -0700

    attempted prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string user = PromptUserName();
+        int number = PromptUserNumber();
+        int sqnum = SquareNumber(number);
+        DisplayResult(user, sqnum);
+    }
+
+    // Functions
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static void PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string user = Console.ReadLine();
+        return user;
+    }
+    static void PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+    static void SquareNumber(int num)
+    {
+        int sqnum = num*num;
+        return sqnum;
+    }
+    static void DisplayResult(string user, int sqnum)
+    {
+        Console.WriteLine($"{user}, the square of your number is {sqnum}.");
     }
 }
