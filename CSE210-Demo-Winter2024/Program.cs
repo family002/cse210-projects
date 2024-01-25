e1c65496e9e3268ae8bd6f41cb9719a3745b65bf
family002 <adama8001@outlook.com>
2024-01-24 18:13:03 -0700

    Created demo and attempted W03 Prepare

---
+++ CSE210-Demo-Winter2024/Program.cs
@@ -0,0 +1,15 @@
+﻿Person person1 = new Person();
+
+person1._givenName = "Jim";
+person1._familyName = "Burton";
+
+person1.ShowEasternName();
+person1.ShowWesternName();
+
+Person person2 = new Person();
+
+person2._givenName = "tom";
+person2._familyName = "bergeron";
+
+person2.ShowEasternName();
+person2.ShowWesternName();
