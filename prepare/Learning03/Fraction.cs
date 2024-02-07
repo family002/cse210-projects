8753e466a35ee09ab01bac7f96c44b79b51c0ed1
family002 <adama8001@outlook.com>
2024-02-06 17:06:03 -0700

    Finished W03 Prepare

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,51 @@
+using System.Formats.Asn1;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string fractionanswer = $"{_top}/{_bottom}";
+        return fractionanswer;
+    }
+    public double GetDecimalValue()
+    {
+        double decimalanswer = (double)_top/(double)_bottom;
+        return decimalanswer;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,23 @@
-using System;
+Fraction fraction1 = new Fraction();
+string f1 = fraction1.GetFractionString();
+double d1 = fraction1.GetDecimalValue();
+Console.WriteLine(f1);
+Console.WriteLine(d1);
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
-    }
-}
+Fraction fraction2 = new Fraction(5);
+string f2 = fraction2.GetFractionString();
+double d2 = fraction2.GetDecimalValue();
+Console.WriteLine(f2);
+Console.WriteLine(d2);
+
+Fraction fraction3 = new Fraction(3, 4);
+string f3 = fraction3.GetFractionString();
+double d3 = fraction3.GetDecimalValue();
+Console.WriteLine(f3);
+Console.WriteLine(d3);
+
+Fraction fraction4 = new Fraction(1, 3);
+string f4 = fraction4.GetFractionString();
+double d4 = fraction4.GetDecimalValue();
+Console.WriteLine(f4);
+Console.WriteLine(d4);
